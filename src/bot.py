5ac7484ef0dd3ebb4d19cf17dde9383716632010
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2022-09-02 23:12:23 +0200

    remove admin ext

--- src/bot.py
+++ src/bot.py
@@ -31,7 +31,7 @@ class MP2IBot(commands.Bot):
             help_command=None,
         )
 
-        self.extensions_names: list[str] = ["cts", "admin"]
+        self.extensions_names: list[str] = ["cts"]  # , "admin"]
 
     async def setup_hook(self) -> None:
         await self.load_extensions()
