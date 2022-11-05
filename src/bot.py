9d803229f174861374f26a2c1dc851f79863a05f
Thomas <thomas.matzinger@etu.unistra.fr>
2022-11-05 20:37:57 +0100

    Les mentions
    Pour que le bot puisse mentionner les gens quand on leurs souhaite joyeux anniverssaire

--- src/bot.py
+++ src/bot.py
@@ -31,7 +31,12 @@ class MP2IBot(commands.Bot):
             tree_cls=CustomCommandTree,
             member_cache_flags=discord.MemberCacheFlags.all(),
             chunk_guilds_at_startup=True,
-            allowed_mentions=discord.AllowedMentions.none(),
+            allowed_mentions=discord.AllowedMentions(
+                users=True,
+                everyone=False,
+                roles=False,
+                replied_user=False,
+            ),
             intents=discord.Intents.all(),
             help_command=None,
         )
