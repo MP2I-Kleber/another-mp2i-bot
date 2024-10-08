5ef24fd7be0af73077f75a11c88fa13c15706268
Syycorax <54723548+Syycorax@users.noreply.github.com>
2024-10-08 15:51:09 +0200

    fix: ratio

--- src/cogs/pin.py
+++ src/cogs/pin.py
@@ -12,14 +12,24 @@ if TYPE_CHECKING:
 
 
 class Pin(Cog):
-    @app_commands.context_menu(name="Épingler/Désépingler")
-    async def pin(interaction: Interaction, message: Message):
-        if message.pinned():
+    def __init__(self, bot: FISABot) -> None:
+        self.bot = bot
+
+        self.bot.tree.add_command(
+            app_commands.ContextMenu(
+                name="Epingler/Desepingler",
+                callback=self.pin,
+            )
+        )
+
+    async def pin(self, interaction: Interaction, message: Message):
+        if message.pinned:
             await message.unpin()
             await interaction.response.send_message(f"Message {message.jump_url} désépinglé.", ephemeral=True)
         else:
             await message.pin()
             await interaction.response.defer()
+
 
 async def setup(bot: FISABot) -> None:
     await bot.add_cog(Pin(bot))
