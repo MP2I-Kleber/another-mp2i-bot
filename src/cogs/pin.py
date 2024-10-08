0d4214529721a5c9a50afc59c84dd0c6eb45e7d5
Syycorax <54723548+Syycorax@users.noreply.github.com>
2024-10-08 15:26:48 +0200

    feat: Added pin context

---
+++ src/cogs/pin.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from discord import app_commands
+from discord.ext.commands import Cog  # pyright: ignore[reportMissingTypeStubs]
+
+if TYPE_CHECKING:
+    from discord import Interaction, Message
+
+    from bot import FISABot
+
+
+class Pin(Cog):
+    @app_commands.context_menu(name="Épingler/Désépingler")
+    async def pin(interaction: Interaction, message: Message):
+        if message.pinned():
+            await message.unpin()
+            await interaction.response.send_message("Message désépinglé.",ephemeral=True)
+        else:
+            await message.pin()
+        await interaction.response.defer()
+
+async def setup(bot: FISABot) -> None:
+    await bot.add_cog(Pin(bot))
