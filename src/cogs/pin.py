6d18a4dc057ec758c320149e3ecbefb6e525b5dd
Pierre <47398145+AiroPi@users.noreply.github.com>
2024-10-08 15:58:35 +0200

    Update src/cogs/pin.py

--- src/cogs/pin.py
+++ src/cogs/pin.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from discord import app_commands, Message
+from discord import Message, app_commands
 from discord.ext.commands import Cog  # pyright: ignore[reportMissingTypeStubs]
 
 if TYPE_CHECKING:
